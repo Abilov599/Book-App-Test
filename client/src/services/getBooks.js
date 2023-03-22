256a46a5e6733ab48d6a3f07fdfcc5cc63cfe32c
Abilov Jeyhun <abilovv599@gmail.com>
2023-03-22 12:37:32 +0400

    header almost done

--- client/src/services/getBooks.js
+++ client/src/services/getBooks.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyAaf6cfMY_ap0kDbLvhqcE-62vSObQ-3F4";
 
-const fetchBooks = createAsyncThunk("fetchBooks", async () => {
+const fetchBooks = createAsyncThunk("fetchBooks", async (book) => {
   const res = await axios.get(
-    `https://www.googleapis.com/books/v1/volumes?q=search+terms&key=${API_KEY}`
+    `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${API_KEY}`
   );
   return res.data;
 });
--- client/src/views/home/index.jsx
+++ client/src/views/home/index.jsx
@@ -1,17 +1,115 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./index.scss";
 import fetchBooks from "../../services/getBooks";
+import { Input, Select } from "antd";
+const { Search } = Input;
 
 const Home = () => {
+  const [count, setCount] = useState(10);
+  const [subject, setSubject] = useState("all");
+  const [order, setOrder] = useState("relevance");
   const dispatch = useDispatch();
   const { loading, error, data } = useSelector((state) => state.getBooksSlice);
 
   useEffect(() => {
-    dispatch(fetchBooks());
+    dispatch(
+      fetchBooks(`subject:${subject}&maxResults=${count}&orderBy=${order}`)
+    );
   }, [dispatch]);
 
-  return loading ? <h1>Loading...</h1> : console.log(data?.items);
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
+  return (
+    <main>
+      <section id="search-filter">
+        <div className="container">
+          <h1>Search for Book</h1>
+          <div className="search">
+            <Search
+              placeholder="Book name"
+              enterButton="Search"
+              size="large"
+              loading={loading}
+            />
+          </div>
+          <div className="filter">
+            <div className="categories">
+              <label htmlFor="categories">Categories</label>
+              <Select
+                name="categories"
+                id="categories"
+                defaultValue="all"
+                style={{
+                  width: 120,
+                }}
+                onChange={handleChange}
+                options={[
+                  {
+                    value: "all",
+                    label: "All",
+                  },
+                  {
+                    value: "art",
+                    label: "Art",
+                  },
+                  {
+                    value: "biography",
+                    label: "Biography",
+                  },
+                  {
+                    value: "computers",
+                    label: "Computers",
+                  },
+                  {
+                    value: "history",
+                    label: "History",
+                  },
+                  {
+                    value: "medical",
+                    label: "Medical",
+                  },
+                  {
+                    value: "poetry",
+                    label: "Poetry",
+                  },
+                ]}
+              />
+            </div>
+            <div className="sort">
+              <label htmlFor="sort">Sorting by</label>
+              <Select
+                name="sort"
+                id="sort"
+                defaultValue="Relevance"
+                style={{
+                  width: 120,
+                }}
+                onChange={handleChange}
+                options={[
+                  {
+                    value: "relevance",
+                    label: "Relevance",
+                  },
+                  {
+                    value: "newest",
+                    label: "Newest",
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="books">
+        <div className="container">
+          
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
